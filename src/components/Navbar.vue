e2c4c1afb9166498053767111158dfe34d116cab
Chayenne Bien-Aimé <98048112+chayenneBA@users.noreply.github.com>
2023-11-22 13:37:35 -0500

    init layout

---
+++ src/components/Navbar.vue
@@ -0,0 +1,58 @@
+<template>
+    <ul>
+  <li><a class="dropdown">File</a>
+       <div class = "dropContent">
+        <p>Load File</p>
+        <p>Save Pattern</p>
+        <p>Patterns</p>
+       </div>
+</li>
+  <li><a >Test</a></li>
+
+</ul>
+  </template>
+
+<style>
+ul {
+  list-style-type: none;
+  margin: 0;
+  padding: 0;
+  overflow: hidden;
+  background-color: #333;
+}
+
+li {
+  float: left;
+}
+
+li a {
+  display: block;
+  color: white;
+  text-align: center;
+  padding: 14px 16px;
+  text-decoration: none;
+}
+
+li a:hover {
+  background-color: #111;
+}
+.dropdown:hover {
+  position: relative;
+  display: inline-block;
+}
+
+.dropContent {
+  display: none;
+  position: absolute;
+  background-color: #f9f9f9;
+  min-width: 160px;
+  box-shadow: 0px 8px 16px 0px rgba(0,0,0,0.2);
+  padding: 12px 16px;
+  z-index: 1;
+}
+
+.dropdown:hover .dropdown-content {
+  display: block;
+}
+
+</style>
---
+++ src/components/Table.vue
@@ -0,0 +1,284 @@
+<template>
+<div table = "board" >
+    <table id="table" class="table" cellspacing="0" style="width:75% height:25%">
+      <tr >
+        <td >
+          <label >0 </label>
+        </td>
+        <td >
+          <label>0 </label>
+        </td>
+        <td >
+          <label>0 </label>
+        </td>
+        <td >
+          <label >0 </label>
+        </td>
+        <td >
+          <label >0 </label>
+        </td>
+        <td >
+          <label >0 </label>
+        </td>
+        <td >
+          <label >0 </label>
+        </td>
+        <td >
+          <label >0 </label>
+        </td>
+        <td >
+          <label >0 </label>
+        </td>
+      </tr> 
+      <tr >
+        <td >
+          <label >0 </label>
+        </td>
+        <td >
+          <label>0 </label>
+        </td>
+        <td >
+          <label>0 </label>
+        </td>
+        <td >
+          <label >0 </label>
+        </td>
+        <td >
+          <label >0 </label>
+        </td>
+        <td >
+          <label >0 </label>
+        </td>
+        <td >
+          <label >0 </label>
+        </td>
+        <td >
+          <label >0 </label>
+        </td>
+        <td >
+          <label >0 </label>
+        </td>
+      </tr> 
+      <tr >
+        <td >
+          <label >0 </label>
+        </td>
+        <td >
+          <label>0 </label>
+        </td>
+        <td >
+          <label>0 </label>
+        </td>
+        <td >
+          <label >0 </label>
+        </td>
+        <td >
+          <label >0 </label>
+        </td>
+        <td >
+          <label >0 </label>
+        </td>
+        <td >
+          <label >0 </label>
+        </td>
+        <td >
+          <label >0 </label>
+        </td>
+        <td >
+          <label >0 </label>
+        </td>
+      </tr> 
+      <tr >
+        <td >
+          <label >0 </label>
+        </td>
+        <td >
+          <label>0 </label>
+        </td>
+        <td  >
+          <label>0 </label>
+        </td>
+        <td >
+          <label >0 </label>
+        </td>
+        <td >
+          <label >0 </label>
+        </td>
+        <td >
+          <label >0 </label>
+        </td>
+        <td >
+          <label >0 </label>
+        </td>
+        <td >
+          <label >0 </label>
+        </td>
+        <td >
+          <label >0 </label>
+        </td>
+      </tr> 
+      <tr >
+        <td >
+          <label >0 </label>
+        </td>
+        <td  >
+          <label>0 </label>
+        </td>
+        <td  >
+          <label>0 </label>
+        </td>
+        <td >
+          <label >0 </label>
+        </td>
+        <td >
+          <label >0 </label>
+        </td>
+        <td >
+          <label >0 </label>
+        </td>
+        <td >
+          <label >0 </label>
+        </td>
+        <td >
+          <label >0 </label>
+        </td>
+        <td >
+          <label >0 </label>
+        </td>
+      </tr> 
+      <tr >
+        <td >
+          <label >0 </label>
+        </td>
+        <td  >
+          <label>0 </label>
+        </td>
+        <td  >
+          <label>0 </label>
+        </td>
+        <td >
+          <label >0 </label>
+        </td>
+        <td >
+          <label >0 </label>
+        </td>
+        <td >
+          <label >0 </label>
+        </td>
+        <td >
+          <label >0 </label>
+        </td>
+        <td >
+          <label >0 </label>
+        </td>
+        <td >
+          <label >0 </label>
+        </td>
+      </tr> 
+      <tr >
+        <td >
+          <label >0 </label>
+        </td>
+        <td  >
+          <label>0 </label>
+        </td>
+        <td  >
+          <label>0 </label>
+        </td>
+        <td >
+          <label >0 </label>
+        </td>
+        <td >
+          <label >0 </label>
+        </td>
+        <td >
+          <label >0 </label>
+        </td>
+        <td >
+          <label >0 </label>
+        </td>
+        <td >
+          <label >0 </label>
+        </td>
+        <td >
+          <label >0 </label>
+        </td>
+      </tr> 
+      <tr >
+        <td >
+          <label >0 </label>
+        </td>
+        <td  >
+          <label>0 </label>
+        </td>
+        <td  >
+          <label>0 </label>
+        </td>
+        <td >
+          <label >0 </label>
+        </td>
+        <td >
+          <label >0 </label>
+        </td>
+        <td >
+          <label >0 </label>
+        </td>
+        <td >
+          <label >0 </label>
+        </td>
+        <td >
+          <label >0 </label>
+        </td>
+        <td >
+          <label >0 </label>
+        </td>
+      </tr> 
+      <tr >
+        <td >
+          <label >0 </label>
+        </td>
+        <td  >
+          <label>0 </label>
+        </td>
+        <td  >
+          <label>0 </label>
+        </td>
+        <td >
+          <label >0 </label>
+        </td>
+        <td >
+          <label >0 </label>
+        </td>
+        <td >
+          <label >0 </label>
+        </td>
+        <td >
+          <label >0 </label>
+        </td>
+        <td >
+          <label >0 </label>
+        </td>
+        <td >
+          <label >0 </label>
+        </td>
+      </tr> 
+      </table>
+  </div>
+</template>
+
+<style scoped>
+table{
+    border: 1px solid black; 
+    width: 500px;
+    height: 500px;
+    justify-content: center;
+}
+
+th{
+    border: 1px solid black; 
+}
+td{
+    border: 1px solid black; 
+    text-align: center;
+}
+</style>
