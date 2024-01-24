b7ec642eb72f5f1490dd7d373cfe398a3c23446b
Samuel Proulx <samuelproulx26@gmail.com>
2024-01-24 17:29:40 -0500

    wrong folder

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,20 +1,64 @@
 <template>
-  <ul>
-    <li class="dropdown">
-      <a class="dropButton">File</a>
-      <div class = "dropContent">
-        <button class="navbar-button">Load File</button>
-        <button class="navbar-button">Patterns</button>
-        <button class="navbar-button">Save Pattern</button>
+  <div class="navbar-container">
+    <div class="navbar-elements">
+      <div class="dropdown">
+        <button class="navbar-button dropButton">File</button>
+        <div class="dropContent">
+          <button class="dropdown-button">Load File</button>
+          <button class="dropdown-button">Patterns</button>
+          <button class="dropdown-button">Save Pattern</button>
+        </div>
       </div>
-    </li>
-    <li><a>Connect</a></li>
-    <li><a >Test</a></li>
-  </ul>
+      <button class="navbar-button">Help</button>
+    </div>
+    <div class="navbar-select-container">
+      <Select></Select>
+    </div>
+    <button @click="list()" class="navbar-button">List</button>
+  </div>
 </template>
 
+<script lang="ts" setup>
+import { invoke } from "@tauri-apps/api";
+import Select from "./Select.vue";
+
+function list() {
+  invoke('list_serial_ports').then((p: any) => {
+    console.log(p);
+  });
+  // console.log("list");
+}
+</script>
+
 <style>
+.navbar-container {
+  overflow: hidden;
+  background-color: #333;
+  height: 54px;
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+}
+
+.navbar-elements {
+  display: flex;
+  flex-direction: row;
+}
+
 .navbar-button {
+  background: none;
+  border: none;
+  box-shadow: none;
+  border-radius: 0;
+  color: white;
+  height: 100%;
+}
+
+.navbar-button:hover {
+  color: rgb(0, 208, 255);
+}
+
+.dropdown-button {
   width: 100%;
   background: none;
   border: none;
@@ -23,11 +67,11 @@
   border-radius: 0;
 }
 
-.navbar-button:hover {
+.dropdown-button:hover {
   background-color: #eee;
 }
 
-.navbar-button:active {
+.dropdown-button:active {
   background-color: #aaa;
 }
 
@@ -35,23 +79,10 @@ ul {
   list-style-type: none;
   margin: 0;
   padding: 0;
-  overflow: hidden;
-  background-color: #333;
 }
 
 li {
   float: left;
-}
-li a, .dropButton {
-  display: block;
-  color: white;
-  text-align: center;
-  padding: 14px 16px;
-  text-decoration: none;
-}
-
-li a:hover, .dropdown:hover .dropButton {
-  background-color: #111;
 }
 
 .dropContent {
@@ -59,7 +90,7 @@ li a:hover, .dropdown:hover .dropButton {
   position: absolute;
   background-color: #f9f9f9;
   min-width: 160px;
-  box-shadow: 0px 8px 16px 0px rgba(0,0,0,0.2);
+  box-shadow: 0px 8px 16px 0px rgba(0, 0, 0, 0.2);
   z-index: 1;
 }
 
@@ -71,10 +102,15 @@ li a:hover, .dropdown:hover .dropButton {
   text-align: left;
 }
 
-.dropContent a:hover {background-color: #f1f1f1;}
+.dropContent a:hover {
+  background-color: #f1f1f1;
+}
 
 .dropdown:hover .dropContent {
   display: block;
 }
 
+.navbar-select-container {
+  margin-right: 12px;
+}
 </style>
---
+++ src/components/Select.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="select-container">
+    <select class="navbar-button">
+      <option>Teensy</option>
+      <option>Arduino</option>
+    </select>
+  </div>
+</template>
+
+<style>
+/* .select-container {
+  position: relative;
+  font-family: Arial;
+}
+
+.select-container select {
+  display: none;
+}
+
+.select-selected {
+  background-color: DodgerBlue;
+}
+
+.select-selected:after {
+  position: absolute;
+  content: "";
+  top: 14px;
+  right: 10px;
+  width: 0;
+  height: 0;
+  border: 6px solid transparent;
+  border-color: #fff transparent transparent transparent;
+} */
+</style>
