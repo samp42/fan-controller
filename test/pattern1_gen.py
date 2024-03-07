0e461c118be7545fb67e7f3e38f0d6df94283505
Samuel Proulx <samuelproulx26@gmail.com>
2024-03-06 19:03:51 -0500

    csv

--- test/pattern1_gen.py
+++ test/pattern1_gen.py
@@ -1,19 +1,26 @@
-# generates a sine wave that travels vertically
+# generates a sine wave (TODO: that travels vertically)
 
 import numpy as np
 import pandas as pd
+import matplotlib.pyplot as plt
 
-time_values = np.arange(0, 2 * np.pi, 0.001)
+dt = 0.05 # s
+f = 1 # Hz
 
-df = pd.DataFrame({'dt': 0.001 * np.ones_like(time_values)})
+time_values = np.arange(0, 1 / f, dt)
 
-# for t in time_values:
-#     sine_wave = 100 * np.sin(t)
-#     for i in range(1, 82):
-#         fan_label = f'fan{i}'
-#         df[fan_label] = sine_wave
+df = pd.DataFrame({'dt': dt * np.ones_like(time_values)})
 
-# df.to_csv('pattern1.csv', index=False)
+y = []
 
 for t in time_values:
-    print(np.sin(t / (2*np.pi)))
+    y.append(int(-50 * np.cos(2 * np.pi * t) + 50))
+
+for i in range(1, 82):
+    fan_label = f'fan{i}'
+    df[fan_label] = y
+
+df.to_csv('pattern1.csv', index=False)
+
+plt.plot(time_values, y)
+plt.show()
