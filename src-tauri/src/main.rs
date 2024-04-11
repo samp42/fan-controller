b22a5d7f58898a7fdb049c40128469a7e18293f6
Samuel Proulx <samuelproulx26@gmail.com>
2024-04-11 18:31:54 -0400

    timing code'

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,6 +1,7 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
+use core::time;
 use std::{
     sync::{Arc, Mutex},
     time::Duration,
@@ -8,6 +9,9 @@ use std::{
 
 use serde::{Deserialize, Serialize};
 use tauri::Manager;
+
+use serialport::SerialPort;
+use std::time::SystemTime;
 
 #[derive(Debug, Deserialize)]
 struct InstantProfile {
@@ -114,13 +118,11 @@ fn run_pattern(
             let mut running = state.run.lock().unwrap();
 
             if *running {
-                // TODO: run logic
-
                 match grid_value {
                     Some(ref grid) => {
                         println!("Running grid");
                         set_grid_to_speed(&cloned_port, &grid);
-                    },
+                    }
                     None => match profiles {
                         Some(ref profiles) => {
                             println!("Running profile");
@@ -163,13 +165,196 @@ fn stop_pattern(state: tauri::State<PatternArc>, port: String) {
     drop(run);
 }
 
+#[tauri::command]
+fn get_timing(_state: tauri::State<PatternArc>, port: String) {
+    println!("Getting timing data");
+    let duration = 50;
+    let step_delay = duration / 40;
+
+    // let mut run = state.run.lock().unwrap();
+
+    let mut port = serialport::new(port, 115200)
+        .timeout(std::time::Duration::from_millis(1))
+        .open()
+        .expect("Failed to open port");
+
+    loop {
+        for speed in 0..101 {
+            let start_marker: [u8; 2] = [0x00, 0xFF];
+            let end_marker: [u8; 2] = [0xFF, 0x00];
+            let mut data_packet: Vec<u8> = Vec::new();
+
+            data_packet.extend_from_slice(&start_marker);
+
+            let duration_since_epoch = SystemTime::now()
+                .duration_since(SystemTime::UNIX_EPOCH)
+                .unwrap();
+            let timestamp_nanos = duration_since_epoch.as_micros();
+            // write timestamp_nanos to the data packet
+            let timestamp_bytes = timestamp_nanos.to_le_bytes();
+            // println!("{:?}", timestamp_nanos);
+            // println!("Timestamp bytes: {:?}", timestamp_bytes);
+            data_packet.extend_from_slice(&timestamp_bytes);
+
+            for _ in 0..81 {
+                let pwm_value = (speed as f32 / 100.0 * 4095.0) as u16;
+                let high_byte = (pwm_value >> 8) as u8;
+                let low_byte = (pwm_value & 0xFF) as u8;
+
+                data_packet.push(high_byte);
+                data_packet.push(low_byte);
+            }
+
+            data_packet.extend_from_slice(&end_marker);
+
+            let duration_since_epoch = SystemTime::now()
+                .duration_since(SystemTime::UNIX_EPOCH)
+                .unwrap();
+            let dt = duration_since_epoch.as_micros();
+
+            port.write_all(&data_packet)
+                .expect("Should have written data");
+            let mut buffer: Vec<u8> = vec![0; 16]; // Buffer for reading 9 bytes
+            let bytes_read_opt = port.read(buffer.as_mut_slice());
+            // println!("Wrote Buffer: {:?}", buffer);
+            let duration_since_epoch = SystemTime::now()
+                .duration_since(SystemTime::UNIX_EPOCH)
+                .unwrap();
+            println!(
+                "{:?}",
+                (duration_since_epoch.as_micros() - dt)
+            );
+            match bytes_read_opt {
+                Ok(bytes_read) => {
+                    if bytes_read >= 16 {
+                        // if buffer contains 0xFE
+                        // println!("Read Buffer: {:?}", buffer);
+                        // println!("bytes read: {:?}", bytes_read);
+
+                        if buffer[0] == 0xFE {
+                            let receive_duration_bytes: &[u8] = &buffer[1..16];
+                            let receive_duration =
+                                u32::from_le_bytes(receive_duration_bytes.try_into().unwrap());
+
+                            // println!("Receive Duration: {}", receive_duration);
+                            // println!("Sent bytes: {:?}", timestamp_bytes);
+                            // println!("Receive Duration bytes: {:?}", receive_duration_bytes);
+
+                            // if buffer[] == 0xFF {
+                            //     let pwm_set_duration_bytes = &buffer[6..];
+                            //     let pwm_set_duration =
+                            //         u32::from_le_bytes(pwm_set_duration_bytes.try_into().unwrap());
+
+                            //     // Process the received data
+                            //     println!("Receive Duration: {}", receive_duration);
+                            //     println!("PWM Set Duration: {}", pwm_set_duration);
+                            // }
+                        }
+
+                        // println!("If bytes_read >= 17");
+                    }
+                    // println!("bytes read: {:?}", bytes_read);
+                }
+                Err(_) => println!("Failed to read from port"),
+            }
+            std::thread::sleep(Duration::from_secs(step_delay));
+            // read_timing_data(port);
+        }
+
+        // for speed in (0..101).rev() {
+        //     let start_marker: [u8; 2] = [0x00, 0xFF];
+        //     let end_marker: [u8; 2] = [0xFF, 0x00];
+        //     let mut data_packet: Vec<u8> = Vec::new();
+
+        //     data_packet.extend_from_slice(&start_marker);
+
+        //     for _ in 0..81 {
+        //         let pwm_value = (speed as f32 / 100.0 * 4095.0) as u16;
+        //         let high_byte = (pwm_value >> 8) as u8;
+        //         let low_byte = (pwm_value & 0xFF) as u8;
+
+        //         data_packet.push(high_byte);
+        //         data_packet.push(low_byte);
+        //     }
+
+        //     data_packet.extend_from_slice(&end_marker);
+        //     port.write_all(&data_packet)
+        //         .expect("Should have written data");
+
+        //     let mut buffer: Vec<u8> = vec![0; 9]; // Buffer for reading 9 bytes
+        //     let bytes_read_opt = port.read(buffer.as_mut_slice());
+        //     match bytes_read_opt {
+        //         Ok(bytes_read) => {
+        //             if bytes_read >= 9 {
+        //                 if buffer[0] == 0xFE {
+        //                     let receive_duration_bytes = &buffer[1..5];
+        //                     let receive_duration =
+        //                         u32::from_le_bytes(receive_duration_bytes.try_into().unwrap());
+
+        //                     if buffer[5] == 0xFF {
+        //                         let pwm_set_duration_bytes = &buffer[6..];
+        //                         let pwm_set_duration =
+        //                             u32::from_le_bytes(pwm_set_duration_bytes.try_into().unwrap());
+
+        //                         // Process the received data
+        //                         println!("Receive Duration: {}", receive_duration);
+        //                         println!("PWM Set Duration: {}", pwm_set_duration);
+        //                     }
+        //                 }
+        //             }
+        //             println!("bytes read: {:?}", bytes_read);
+        //         }
+        //         Err(_) => println!("Failed to read from port"),
+        //     }
+        //     std::thread::sleep(Duration::from_secs(step_delay));
+        // }
+    }
+}
+
+fn read_timing_data(port: Box<dyn SerialPort>) {
+    // let mut buffer: Vec<u8> = vec![0; 9]; // Buffer for reading 9 bytes
+    // let bytes_read_opt = port.read(buffer.as_mut_slice());
+
+    // let bytes = port.bytes_to_read();
+
+    // match bytes {
+    //     Ok(bytes) => {
+    //         // let mut buffer: Vec<u8> = vec![0; bytes];
+    //         // let bytes_read = port.read(buffer.as_mut_slice()).unwrap();
+
+    //         println!("{:?}", bytes);
+    //     }
+    //     Err(_) => println!("Failed to read from port"),
+    // }
+
+    // match bytes_read_opt {
+    //     Ok(bytes_read) => {
+    //         if bytes_read >= 9 {
+    //             if buffer[0] == 0xFE {
+    //                 let receive_duration_bytes = &buffer[1..5];
+    //                 let receive_duration = u32::from_le_bytes(receive_duration_bytes.try_into().unwrap());
+
+    //                 if buffer[5] == 0xFF {
+    //                     let pwm_set_duration_bytes = &buffer[6..];
+    //                     let pwm_set_duration = u32::from_le_bytes(pwm_set_duration_bytes.try_into().unwrap());
+
+    //                     // Process the received data
+    //                     println!("Receive Duration: {}", receive_duration);
+    //                     println!("PWM Set Duration: {}", pwm_set_duration);
+    //                 }
+    //             }
+    //         }
+    //         println!("bytes read: {:?}", bytes_read);
+    //     }
+    //     Err(_) => println!("Failed to read from port"),
+    // }
+
+    // println!("{:?}", timing_data);
+}
+
 fn main() {
     let pattern = PatternArc {
         run: Arc::new(Mutex::new(false)),
-        // profile: Arc::new(Mutex::new(InstantProfile {
-        //     fans: vec![0; 81],
-        //     delta_t: 0,
-        // })),
     };
 
     let result = tauri::Builder::default()
@@ -177,144 +362,13 @@ fn main() {
         .invoke_handler(tauri::generate_handler![
             list_serial_ports,
             run_pattern,
-            stop_pattern
+            stop_pattern,
+            get_timing
         ])
         .build(tauri::generate_context!());
 
     match result {
         Ok(app) => {
-            // Get a handle to the Tauri manager
-            // let manager: tauri::State<'_, Pattern> = app.state();
-
-            //         std::thread::spawn(move || {
-            //             // get port
-            //             // let port = manager.port.lock().unwrap();
-
-            //             let mut port = serialport::new(manager.port.lock().unwrap(), 115200)
-            //                 .timeout(std::time::Duration::from_millis(1))
-            //                 .open()
-            //                 .expect("Failed to open port");
-
-            //             let mut counter: u8 = 0; // 0 to 100
-
-            //             loop {
-            //                 // println!("{:?}", cloned_manager.run.lock().unwrap().to_string());
-            //                 println!("running loop");
-
-            //                 let start_marker: [u8; 2] = [0x00, 0xFF];
-            //                 let end_marker: [u8; 2] = [0xFF, 0x00];
-            //                 let mut data_packet: Vec<u8> = Vec::new();
-
-            //                 data_packet.extend_from_slice(&start_marker);
-
-            //                 for i in 0..81 {
-            //                     let pwm_value = (0 as f32 / 100.0 * 4095.0) as u16;
-            //                     let high_byte = (pwm_value >> 8) as u8;
-            //                     let low_byte = (pwm_value & 0xFF) as u8;
-
-            //                     data_packet.push(high_byte);
-            //                     data_packet.push(low_byte);
-            //                 }
-
-            //                 data_packet.extend_from_slice(&end_marker);
-
-            //                 println!("{:?}", data_packet);
-
-            //                 port.write_all(&data_packet)
-            //                     .expect("Failed to write to port");
-
-            //                 // let running = pattern.run.lock().unwrap();
-
-            //                 // println!("Pattern running: {:?}", running);
-
-            //                 // drop(running);
-
-            //                 // Simulate some delay between iterations
-            //                 std::thread::sleep(std::time::Duration::from_millis(100));
-
-            //                 println!("Counter: {:?}", counter);
-
-            //                 if (counter == 100) {
-            //                     counter = 0;
-            //                 } else {
-            //                     counter += 1;
-            //                 }
-            //             }
-
-            //             // loop {
-            //             //     //                 tep_delay = duration / 40.0
-            //             //     // for speed in range(101):
-            //             //     //     send_fan_speeds(speed)
-            //             //     //     time.sleep(step_delay)
-            //             //     //     read_timing_data()
-
-            //             //     // for speed in range(100, -1, -1):
-            //             //     //     send_fan_speeds(speed)
-            //             //     //     time.sleep(step_delay)
-            //             //     //     read_timing_data()
-
-            //             //     let step_delay = 0.125;
-
-            //             //     for speed in 0..101 {
-            //             //         let start_marker: [u8; 2] = [0x00, 0xFF];
-            //             //         let end_marker: [u8; 2] = [0xFF, 0x00];
-            //             //         let mut data_packet: Vec<u8> = Vec::new();
-
-            //             //         data_packet.extend_from_slice(&start_marker);
-
-            //             //         for _ in 0..81 {
-            //             //             let pwm_value = (speed as f32 / 100.0 * 4095.0) as u16;
-            //             //             let high_byte = (pwm_value >> 8) as u8;
-            //             //             let low_byte = (pwm_value & 0xFF) as u8;
-
-            //             //             data_packet.push(high_byte);
-            //             //             data_packet.push(low_byte);
-            //             //         }
-
-            //             //         data_packet.extend_from_slice(&end_marker);
-
-            //             //         port.write_all(&data_packet)
-            //             //             .expect("Failed to write to port");
-            //             //         std::thread::sleep(std::time::Duration::from_secs_f32(step_delay));
-            //             //         let mut buffer: Vec<u8> = vec![0; 83];
-
-            //             //         port.read_exact(&mut buffer)
-            //             //             .expect("Failed to read from port");
-
-            //             //         println!("{:?}", buffer);
-            //             //     }
-
-            //             //     for speed in (0..101).rev() {
-            //             //         let start_marker: [u8; 2] = [0x00, 0xFF];
-            //             //         let end_marker: [u8; 2] = [0xFF, 0x00];
-            //             //         let mut data_packet: Vec<u8> = Vec::new();
-
-            //             //         data_packet.extend_from_slice(&start_marker);
-
-            //             //         for _ in 0..81 {
-            //             //             let pwm_value = (speed as f32 / 100.0 * 4095.0) as u16;
-            //             //             let high_byte = (pwm_value >> 8) as u8;
-            //             //             let low_byte = (pwm_value & 0xFF) as u8;
-
-            //             //             data_packet.push(high_byte);
-            //             //             data_packet.push(low_byte);
-            //             //         }
-
-            //             //         data_packet.extend_from_slice(&end_marker);
-
-            //             //         port.write_all(&data_packet)
-            //             //             .expect("Failed to write to port");
-            //             //         std::thread::sleep(std::time::Duration::from_secs_f32(step_delay));
-            //             //         let mut buffer: Vec<u8> = vec![0; 83];
-
-            //             //         port.read_exact(&mut buffer)
-            //             //             .expect("Failed to read from port");
-
-            //             //         println!("{:?}", buffer);
-            //             //     }
-            //             // }
-            //         });
-
             app.run(|_app_handle, _event| {});
         }
         Err(_) => println!("error while running tauri application"),
